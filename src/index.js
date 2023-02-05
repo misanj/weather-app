104e47a7a49114e1dcff584c27b3992703c24264
misanj <47555358+misanj@users.noreply.github.com>
2023-02-05 21:49:38 +0100

    Updated time
    This comits adds the current time functionality to the weather app.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10){
+        hours = `0${hours}`;
+    }
+    
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    
+    let days = [
+        'Sunday', 
+        'Monday', 
+        'Tuesday', 
+        'Wednesday', 
+        'Thursday', 
+        'Friday', 
+        'Saturday'
+    ];
+
+    let day = days[date.getDay()];
+    return `${day} ${hours}: ${minutes}`;
+}
+
 function displayTemperature(response) {
     //   console.log(response.data.temperature.current);
     let tempElement = document.querySelector("#temp");
@@ -6,6 +32,7 @@ function displayTemperature(response) {
     let iconElement = document.querySelector("#icon");
     let humidityElement = document.querySelector("#humidElement");
     let windElement = document.querySelector("#windSpeed");
+    let dateElement = document.querySelector("#date");
     
     tempElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
@@ -13,6 +40,7 @@ function displayTemperature(response) {
     iconElement.innerHTML = response.data.condition.icon_url;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "5acaob54d1tff787538b50f02534ee0a";
